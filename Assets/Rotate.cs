a9967314df04d7ff89cf0a5b0bb2e7c6f3453721
quangvasot <quangvasot@gmail.com>
2022-10-12 21:01:51 +0900

    day 2 commit
    fixed shooting code, player model rotation issues, etc.

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
     public Transform Player;
-    public GameObject Bullet;
+    public GameObject Bullet1;
+    public GameObject Bullet2;
     public float bulletspeed = 30f;
     void Update()   
     {
+        //player stuff
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 0;
         Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-
         Vector3 aimPos = (mousePos - objectPos).normalized;
-
         float Pizangle = Mathf.Atan2(aimPos.y, aimPos.x) * Mathf.Rad2Deg;
-        Player.rotation = Quaternion.Euler(0, 0, Pizangle -90);
+        Player.rotation = Quaternion.Euler(0, 0, Pizangle);
 
+        //shooting stuff
+        Vector3 aimDir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float aimAngle = Mathf.Atan2(aimDir.x, aimDir.y) * Mathf.Rad2Deg;
         if (Input.GetButtonDown("Fire1"))   
         {
-            GameObject bulletclone = Instantiate(Bullet, Player.position, Quaternion.Euler(new Vector3(0, 0, Pizangle -90)));       
-            bulletclone.GetComponent<Rigidbody2D>().velocity = Player.right * bulletspeed ;
+            GameObject bulletclone1 = Instantiate(Bullet1);
+            bulletclone1.transform.position = Player.position;
+            bulletclone1.transform.rotation = Quaternion.identity;
+            bulletclone1.GetComponent<Rigidbody2D>().velocity = Player.right * bulletspeed ;
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            GameObject bulletclone2 = Instantiate(Bullet2);
+            bulletclone2.transform.position = Player.position;
+            bulletclone2.transform.rotation = Quaternion.identity;
+            bulletclone2.GetComponent<Rigidbody2D>().velocity = Player.right * bulletspeed;
         }
     }
 }
